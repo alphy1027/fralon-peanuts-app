d31e7e3b076d2e6c55c9ccb6e0d1cc1f10be3abd
alphy1027 <alphy1027@gmail.com>
2025-04-04 03:54:05 +0300

    Fixed a config file bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      /* '@assets': '/src/assets',
+      '@components': '/src/components',
+      '@config': '/src/config',
+      '@contexts': '/src/contexts',
+      '@hooks': '/src/hooks',
+      '@layouts': '/src/layouts',
+      '@lib': '/src/lib',
+      '@pages': '/src/pages',
+      '@routes': '/src/routes',
+      '@services': '/src/services',
+      '@types': '/src/types',
+      '@utils': '/src/utils', */
+    },
+  },
 });
