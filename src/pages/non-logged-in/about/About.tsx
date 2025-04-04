0df6817a0ad82e142347e7ad438b36cdcb9cfd9f
alphy1027 <alphy1027@gmail.com>
2025-04-04 19:16:34 +0300

    Created identity section in about page

--- src/pages/non-logged-in/about/About.tsx
+++ src/pages/non-logged-in/about/About.tsx
@@ -1,5 +1,6 @@
 import SectionContainter from "@/components/UI-primitives/SectionContainter";
 import SectionTitle from "@/components/UI-primitives/SectionTitle";
+import IdentityCard from "./components/IdentityCard";
 
 const About = () => {
   return (
@@ -7,7 +8,10 @@ const About = () => {
       <SectionTitle>About Us</SectionTitle>
       <section className="bg-primary-light">
         <SectionContainter className="flex flex-wrap items-center justify-center gap-x-8 gap-y-6 xl:gap-x-20 xl:py-10">
-          <img src="null" alt="Company photo" className="bg-brand-white h-56 w-64 md:h-[300px] md:w-[300px]" />
+          <figure>
+            <img src="null" alt="Company photo" className="bg-brand-white h-56 w-64 md:h-[300px] md:w-[300px]" />
+            <figcaption className="sr-only">Peanut Butter Company photo</figcaption>
+          </figure>
           <div className="flex max-w-[500px] flex-col items-start gap-4">
             <SectionTitle>Our Story</SectionTitle>
             <p className="text-caption text-body-default">
@@ -21,17 +25,30 @@ const About = () => {
         </SectionContainter>
       </section>
 
-      <SectionContainter className="">
-        <div className="flex flex-col items-start gap-y-4">
-          <SectionTitle className="text-start">
-            Unveiling Our Identity,
-            <br /> Vision and Values
-          </SectionTitle>
-          <p className="text-caption text-body-default max-w-[590px]">
+      <SectionContainter className="flex flex-wrap items-end justify-center gap-x-14 gap-y-8 sm:gap-y-12 md:gap-y-14">
+        <section className="flex h-fit w-fit flex-col gap-y-8 sm:gap-y-10">
+          <IdentityCard index={1} className="">
             We're passionate about quality peanut butter production. With years of experience in the industry, we have a good understanding about the product and used this to make our products better
             and better along the way. And so we can confidently assure our customers that we offer the best peanut butter in the industry.
-          </p>
-        </div>
+          </IdentityCard>
+
+          <IdentityCard index={2} title="Our Values" className="bg-primary-light">
+            We're passionate about quality peanut butter production. With years of experience in the industry, we have a good understanding about the product and used this to make our products better
+            and better along the way. And so we can confidently assure our customers that we offer the best peanut butter in the industry. And so we can confidently assure our customers that we offer
+          </IdentityCard>
+        </section>
+
+        <section className="flex h-fit w-fit flex-col gap-y-6 sm:gap-y-10">
+          <IdentityCard index={3} title="Our Vission" className="bg-secondary">
+            We're passionate about quality peanut butter production. With years of experience in the industry, we have a good understanding about the product and used this to make our products better
+            and better along the way. And so we can confidently assure our customers that we offer the best peanut butter in the industry.
+          </IdentityCard>
+
+          <IdentityCard index={4} title="Our Mission" className="bg-primary">
+            We're passionate about quality peanut butter production. With years of experience in the industry, we have a good understanding about the product and used this to make our products better
+            and better along the way. And so we can confidently assure our customers that we offer the best peanut butter in the industry.
+          </IdentityCard>
+        </section>
       </SectionContainter>
     </div>
   );
---
+++ src/pages/non-logged-in/about/components/IdentityCard.tsx
@@ -0,0 +1,28 @@
+import SectionTitle from "@/components/UI-primitives/SectionTitle";
+import clsx from "clsx";
+import { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
+
+type IdentityCardProps = {
+  children: ReactNode;
+  title?: string;
+  index: 1 | 2 | 3 | 4;
+  className: string;
+  titleClassName?: string;
+};
+const IdentityCard = ({ children, title, className, titleClassName, index }: IdentityCardProps) => {
+  return (
+    <article className={twMerge(clsx("flex min-h-[180px] max-w-[488px] flex-col justify-center gap-y-2 rounded-2xl p-3 sm:p-4", { "max-w-[590px]": index === 2 || index === 1 }), className)}>
+      {index === 1 && (
+        <SectionTitle className="text-start">
+          Unveiling Our Identity,
+          <br /> Vision and Values
+        </SectionTitle>
+      )}
+      <h4 className={twMerge(clsx("text-body-lg text-body-default font-extrabold", { "text-brand-white": index === 4 }), titleClassName)}>{title}</h4>
+      <p className={twMerge(clsx("text-caption text-body-default sm:text-body", { "text-primary-light": index === 4 }))}>{children}</p>
+    </article>
+  );
+};
+
+export default IdentityCard;
