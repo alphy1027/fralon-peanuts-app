a4a9d17c3ed8b427984573ec6520b1e6d49e432a
alphy1027 <alphy1027@gmail.com>
2025-05-04 15:29:46 +0300

    Created profil/help page

--- src/pages/logged-in/profile/components/help-and-support/index.tsx
+++ src/pages/logged-in/profile/components/help-and-support/index.tsx
@@ -1,5 +1,40 @@
+import BodyText from "@/components/UI-primitives/BodyText";
+import Button from "@/components/UI-primitives/Button";
+import Input from "@/components/UI-primitives/Input";
+import SectionContainter from "@/components/UI-primitives/SectionContainter";
+import SectionTitle from "@/components/UI-primitives/SectionTitle";
+
 const HelpAndSupport = () => {
-  return <div>HelpAndSupport</div>;
+  return (
+    <SectionContainter className="flex flex-col gap-y-12">
+      <SectionTitle className="text-start">Help and Support</SectionTitle>
+      <section className="flex flex-col gap-y-6">
+        <div className="flex flex-col gap-y-2">
+          <h3 className="text-body-default text-heading-4 font-bold">Report Bug</h3>
+          <BodyText>If you encounter a bug, app misbehavior, or something not working properly, please let us know. Your feedback helps us improve.</BodyText>
+        </div>
+        <div className="flex max-w-[400px] flex-col gap-y-4">
+          <Input placeholder="Bug description" />
+          <Input placeholder="Briefly describe the bug you encountered" />
+          <Button size="md" variant="danger" className="self-end">
+            Report Bug
+          </Button>
+        </div>
+      </section>
+      <section className="flex flex-col gap-y-6">
+        <h3 className="text-body-default text-heading-4 font-bold">Support</h3>
+        <BodyText>
+          For any inquiries about our product and services please get in touch with us through our Email fralonpeanuts@gmail.com. You can also get in touch through our help lines +254797000000 or
+          +254735000000 or Whatsapp us through the number +254796000000. You can also reach us through our Contact Form by clicking the Contact us button below
+        </BodyText>
+        <Button size="md">Contact Us</Button>
+      </section>
+      <section className="flex flex-col gap-y-6">
+        <h3 className="text-body-default text-heading-4 font-bold">Help</h3>
+        <BodyText>For any technical inquiries about our platform, you can get in touch with our Technical team of Engineers through the Email scripttag@gmail.com </BodyText>
+      </section>
+    </SectionContainter>
+  );
 };
 
 export default HelpAndSupport;
