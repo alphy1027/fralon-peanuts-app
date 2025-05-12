8aa866ddc16f04d951adc6e982d4caf7d4514ebc
alphy1027 <alphy1027@gmail.com>
2025-05-12 04:20:46 +0300

    Made progress to the homepage

--- src/pages/non-logged-in/home/Home.tsx
+++ src/pages/non-logged-in/home/Home.tsx
@@ -2,39 +2,46 @@ import ProductsSection from "@/components/sections/ProductsSection";
 import TestimonialsSection from "@/components/sections/TestimonialsSection";
 import { useAuth } from "@/context/AuthContext";
 import { Link } from "react-router-dom";
+import AboutSection from "./components/about-section";
+import SectionContainter from "@/components/UI-primitives/SectionContainter";
+import SectionTitle from "@/components/UI-primitives/SectionTitle";
 
 const Home = () => {
   const { user } = useAuth();
 
   return (
-    <section className="flex flex-col gap-4">
-      <h1 className="text-blue-600">Welcome to Fralon Peanuts App.</h1>
-      {user.isAuthenticated ? (
-        <>
-          <h2 className="">Logged in as :</h2>
-          <h3 className="text-emerald-500">Username :: {user.username}</h3>
-          <h3 className="text-emerald-500">UserID :: {user.userId}</h3>
-          <h3 className="text-emerald-500">Roles :: {user.roles[0]}</h3>
-        </>
-      ) : (
-        <h3 className="text-red-400">Please Login to view your information</h3>
-      )}
-
-      <Link to="auth/reset-password" className="text-primary font-semibold underline">
-        Reset Password
-      </Link>
-      <Link to="auth/forgot-password" className="text-primary font-semibold underline">
-        Forgot Password
-      </Link>
-      <Link to="auth/successful-verification" className="text-primary font-semibold underline">
-        Successful Verification
-      </Link>
-      <Link to="auth/successful-reset" className="text-primary font-semibold underline">
-        Successful Reset
-      </Link>
-      <Link to="auth/verification-sent" className="text-primary font-semibold underline">
-        Verification Sent
-      </Link>
+    <section className="flex flex-col gap-10">
+      <SectionContainter className="flex flex-col items-center gap-4">
+        <SectionTitle className="text-blue-600">Welcome to Fralon Peanuts App.</SectionTitle>
+        {user.isAuthenticated ? (
+          <>
+            <h2 className="">Logged in as :</h2>
+            <h3 className="text-emerald-500">Username :: {user.username}</h3>
+            <h3 className="text-emerald-500">UserID :: {user.userId}</h3>
+            <h3 className="text-emerald-500">Roles :: {user.roles[0]}</h3>
+          </>
+        ) : (
+          <h3 className="text-red-400">Please Login to view your information</h3>
+        )}
+      </SectionContainter>
+      <SectionContainter className="flex flex-wrap justify-center gap-4">
+        <Link to="auth/reset-password" className="text-primary font-semibold underline">
+          Reset Password
+        </Link>
+        <Link to="auth/forgot-password" className="text-primary font-semibold underline">
+          Forgot Password
+        </Link>
+        <Link to="auth/successful-verification" className="text-primary font-semibold underline">
+          Successful Verification
+        </Link>
+        <Link to="auth/successful-reset" className="text-primary font-semibold underline">
+          Successful Reset
+        </Link>
+        <Link to="auth/verification-sent" className="text-primary font-semibold underline">
+          Verification Sent
+        </Link>
+      </SectionContainter>
+      <AboutSection />
       <ProductsSection />
       <TestimonialsSection />
     </section>
---
+++ src/pages/non-logged-in/home/components/about-section/index.tsx
@@ -0,0 +1,25 @@
+import BodyText from "@/components/UI-primitives/BodyText";
+import Button from "@/components/UI-primitives/Button";
+import SectionContainter from "@/components/UI-primitives/SectionContainter";
+import SectionTitle from "@/components/UI-primitives/SectionTitle";
+
+const AboutSection = () => {
+  return (
+    <SectionContainter className="flex flex-wrap items-center justify-center gap-12">
+      <div className="flex max-w-[400px] flex-col gap-y-8">
+        <SectionTitle className="text-start">About Our Business</SectionTitle>
+        <BodyText>
+          At [Business Name], we’re more than just a peanut butter company—we’re a family-run business with a passion for crafting high-quality peanut products. From our humble beginnings, we’ve
+          dedicated ourselves to sourcing the finest peanuts and transforming them into products that bring joy to every table.
+        </BodyText>
+        <Button size="md">Learn More</Button>
+      </div>
+      <figure className="w-fit">
+        <img src="none" alt="" className="bg-secondary h-[370px] w-[300px] rounded-lg" />
+        <figcaption className="sr-only">Company photo</figcaption>
+      </figure>
+    </SectionContainter>
+  );
+};
+
+export default AboutSection;
