c7c023627def5304466fa61787121efee91099c1
alphy1027 <alphy1027@gmail.com>
2025-08-18 05:31:10 +0300

    fixed hero section responsiveness

--- src/pages/non-logged-in/home/Home.tsx
+++ src/pages/non-logged-in/home/Home.tsx
@@ -11,8 +11,8 @@ import SectionTitle from "@/components/UI-primitives/SectionTitle";
 const Home = () => {
   return (
     <section className="flex flex-col gap-12">
-      <SectionContainter className="bg-secondary flex items-center justify-center gap-x-10 rounded-sm">
-        <div className="flex w-1/2 flex-col gap-y-2 p-2">
+      <SectionContainter className="bg-secondary flex flex-wrap items-center justify-center gap-x-10 gap-y-6 rounded-sm">
+        <div className="flex flex-col gap-y-3 p-2 md:w-1/2">
           <h1 className="text-heading-2 font-secondary text-primary font-semibold tracking-wider uppercase">
             Crafted for the{" "}
             <span className="text-tertiary">
@@ -24,7 +24,7 @@ const Home = () => {
           <p className="text-slate-700">
             Not Just Any Butter, It's Peanut Perfection. Nutty, But in the Best Way. We offer quality peanut butter products. You can get all our products in a store nearby.
           </p>
-          <div className="flex w-fit items-center justify-center gap-x-6">
+          <div className="flex w-fit flex-wrap items-center justify-center gap-x-6 gap-y-3">
             <Button size="md">View Products</Button>
             <Button variant="outline" size="md">
               Order Now!
